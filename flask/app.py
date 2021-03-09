84c728dd3186975e4571e8932de06d41dd99815f
armonbakhtar1 <arbakhtar@csumb.edu>
2021-03-08 22:57:56 -0800

    fix

--- flask/app.py
+++ flask/app.py
@@ -1,57 +1,63 @@
-from flask import (
-    Flask,
-    g,
-    redirect,
-    render_template,
-    request,
-    session,
-    url_for
-)
+# import the Flask class from the flask module
+from flask import Flask, render_template, redirect, \
+    url_for, request, session, flash
+from functools import wraps
 
-class User:
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
-
-    def __repr__(self):
-        return f'<User: {self.username}>'
-
-users = []
-users.append(User(id=1, username='admin', password='admin'))
-
+# create the application object
 app = Flask(__name__)
-app.secret_key = 'somesecretkeythatonlyishouldknow'
 
-@app.before_request
-def before_request():
-    g.user = None
+# config
+app.secret_key = 'my precious'
 
-    if 'user_id' in session:
-        user = [x for x in users if x.id == session['user_id']][0]
-        g.user = user
-        
 
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+
+# use decorators to link the function to a url
+@app.route('/')
+@login_required
+def home():
+    return render_template('index.html')  # render a template
+    # return "Hello, World!"  # return a string
+
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')  # render a template
+
+
+# route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
     if request.method == 'POST':
-        session.pop('user_id', None)
+        if (request.form['username'] != 'admin') \
+                or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in.')
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
-        username = request.form['username']
-        password = request.form['password']
-        
-        user = [x for x in users if x.username == username][0]
-        if user and user.password == password:
-            session['user_id'] = user.id
-            return redirect(url_for('profile'))
 
-        return redirect(url_for('login'))
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out.')
+    return redirect(url_for('welcome'))
 
-    return render_template('login.html')
 
-@app.route('/profile')
-def profile():
-    if not g.user:
-        return redirect(url_for('login'))
-
-    return render_template('profile.html')
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
