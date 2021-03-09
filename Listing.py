7ce095da1801cd2abfea0dddb21b65c30d568d31
Xenvex <matthewbreyes@gmail.com>
2021-03-09 13:22:36 -0800

    Checkpoint

--- Listing.py
+++ Listing.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
-
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+#Retrieving from Database
+app.config['MYSQL_USER'] = 'la8jsp9yrvvjmecw'
+app.config['MYSQL_PASSWORD'] = 'dz1gz2pnr8awpoc7'
+app.config['MYSQL_DB'] = 'b4rpjxyu117cmkvr'
+app.config['MYSQL_HOST'] = 'pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+mysql = MySQL(app)
+
+#Parse Data, put into List
 
 @app.route("/")
 def home():
     test_list = ['Item1','Item2','Item3','Item4']
-    return render_template('your_view.html', your_list=test_list)
+    return render_template('listing.html', your_list=test_list)
 
 
 
